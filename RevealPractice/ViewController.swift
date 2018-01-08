66716701661c478c7d2cb012fa5ef898b3b12a40
李文静 <Lwj_xyL@163.com>
2018-01-08 16:30:49 +0800

    创建项目

---
+++ RevealPractice/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RevealPractice
+//
+//  Created by 李旭阳 on 2018/1/8.
+//  Copyright © 2018年 李文静. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
